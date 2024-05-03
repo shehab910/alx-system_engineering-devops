f30e6d4d4b9f4ca90d288398affbb7ca52eec2b4
shehab910 <shehabihab@hotmail.com>
2024-05-03 23:00:15 +0300

    feat: Add Puppet code to create '/tmp/school' file with specific properties

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,16 @@
+/**
+ * This Puppet code creates a file named '/tmp/school' with the following properties:
+ * - The file is ensured to be a regular file.
+ * - The file has the mode '0744', which means it has read, write, and execute permissions for the owner,
+ *   and read permissions for the group and others.
+ * - The owner of the file is set to 'www-data'.
+ * - The group of the file is set to 'www-data'.
+ * - The content of the file is set to 'I love Puppet'.
+ */
+file { '/tmp/school':
+	ensure  => file,
+	mode    => '0744',
+	owner   => 'www-data',
+	group   => 'www-data',
+	content => 'I love Puppet',
+}
