e7bfa0c0c437fb64377cdfe7259d8863240e4ef5
shehab910 <shehabihab@hotmail.com>
2024-05-04 13:33:00 +0300

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,15 @@
 # Client SSH configuration file so that you can connect to a server without typing a password
-file { '~/.ssh/config':
-  ensure  => present,
-  content => "
-    Host *
-      IdentityFile ~/.ssh/school
-      PasswordAuthentication no
-  ",
+
+include stdlib
+
+file_line { 'Refuse to authenticate using a password':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Use private key':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school'
 }
