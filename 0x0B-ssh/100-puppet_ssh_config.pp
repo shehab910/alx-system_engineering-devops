5554a713f8fe80ca06632aa78c209e15249eea3f
shehab910 <shehabihab@hotmail.com>
2024-05-04 13:28:22 +0300

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # Client SSH configuration file so that you can connect to a server without typing a passwor
 file { '/home/shehab/.ssh/config':
-  ensure => present,
+  ensure  => present,
   content => "
     Host *
       IdentityFile ~/.ssh/school
